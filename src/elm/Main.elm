219aa6be1763579c7d97956396bf3dc2633d3556
Pietro909 <pietro.grandi.3d@gmail.com>
2016-10-31 16:00:23 +0100

    custom styles and a bit of text more

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -18,18 +18,39 @@ type Msg =
     AMessage
 
 
-type alias Model = String
+type alias Model =
+  { version : String
+  , contributors : List String
+  }
 
 model : Model
-model = "Quickstart version 1.0.0"
+model =
+  { version = "1.0.0"
+  , contributors = [ "Pietro Grandi", "Andrei Toma"]
+  }
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    _ -> model
+  model
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text model ]
+  div [ class "container" ]
+    [ header [ class "row" ]
+      [ div [ class "col-xs-12 col-sm-12 col-md-12 menu" ]
+        [ h1 [] [ text "Elm Quickstart" ] ]
+      ]
+    , section [ class "row" ]
+      [ div [ class "col-xs-12 col-sm-10 col-md-8 col-lg-6" ]
+        [ h2 [] [ text "Elm with Gulp and SASS configured" ]
+        , h3 [] [ text ( "Version " ++ model.version ) ]
+        , p []
+          [ text "Have a look at the "
+          , a [ href "https://github.com/pietro909/elm-quickstart/blob/master/README.md" ] [ text "readme" ]
+          , text " for more information."
+          ]
+        ]
+      ]
+    ]
